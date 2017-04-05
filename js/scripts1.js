651b6987391597a841b5df16485ecb85efbb1b67
Chitroopa Manikkavasagam and Jesse Hoare Evans <jesse@>
2017-04-05 10:09:44 -0700

    Add hold button functionality and  adds turnscore to total score

--- js/scripts1.js
+++ js/scripts1.js
@@ -10,27 +10,33 @@ PigDice.prototype.randomNumber = function() {
   var rNumber = diceArray[Math.floor(Math.random() * diceArray.length)];
   return rNumber;
 }
+PigDice.prototype.holdScore = function() {
+  this.playerScore += this.turnTotal;
+  this.turnTotal = 0;
+  return this.playerScore;
+}
 
 $(document).ready(function(){
-  // var playerScore = 0;
-  // var computerScore =0;
-  // var turnTotal = 0;
+
   var rollScore = 0;
   var playerNameInput = "Bob";
   var computerName = "Watson";
   var newPigDice = new PigDice(playerNameInput, computerName);
-  $("#form-one").submit(function(event){
+  $("#roll").click(function(event){
         event.preventDefault();
     rollScore = newPigDice.randomNumber();
     newPigDice.turnTotal += rollScore;
     if(rollScore === 1)
     {
       newPigDice.turnTotal = 0;
-
     }
-
     console.log(rollScore);
     console.log("turnTotal:" + newPigDice.turnTotal);
 
   });
+  $("#hold-score").click(function(event){
+      event.preventDefault();
+      console.log("Player score:"+newPigDice.holdScore());
+  });
+
 });
